869d435d39f393a7a0617695c4052f0d76d50940
Jatioi <Jsuzibloguen@gmail.com>
2023-10-10 19:37:32 +0800

    Disabled comments/index in routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   root 'welcome#index'
   resources :posts do
-    resources :comments, except: :show
+    resources :comments, except: [:show, :index]
   end
   resources :categories, except: :show
 end
