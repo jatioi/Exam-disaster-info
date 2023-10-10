2a47d134bd95871bfdb3e9111109980943c9107d
Jatioi <Jsuzibloguen@gmail.com>
2023-10-10 12:17:31 +0800

    add: category feature

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,11 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:edit, :update, :destroy]
   def index
     @categories = Category.all
   end
 
   def new
-    @categories = Category.new
+    @category = Category.new
   end
 
   def edit; end
@@ -19,11 +20,10 @@ class CategoriesController < ApplicationController
     end
   end
 
-  def edit; end
-
   def update
+    @category = Category.find(params[:id])
+
     if @category.update(category_params)
-      flash[:notice] = 'Category updated successfully'
       redirect_to categories_path
     else
       render :edit
