f661c8314feb54380ee2a41ebb995d23a3552080
Jatioi <Jsuzibloguen@gmail.com>
2023-10-10 19:56:07 +0800

    Post: Added authentication user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!, except: [:show]
 
   def index
     @posts = Post.includes(:categories).all.page(params[:page]).per(5)
