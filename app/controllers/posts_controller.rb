c3cc987e9bbbee44476ab7934969bab5fe2ab519
Jatioi <Jsuzibloguen@gmail.com>
2023-10-10 12:18:30 +0800

    add: post table

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,47 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
+    @posts = Post.includes(:categories).all
   end
 
   def new
+    @post = Post.new
   end
 
-  def edit
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      flash.now[:alert] = 'Post create failed'
+      render :new
+    end
   end
+  def show; end
+  def edit; end
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:notice] = 'Post update successfully'
+      redirect_to posts_path
+    else
+      flash.now[:alert] = 'Post failed to update'
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  def destroy
+    @post.destroy
+    flash[:notice] = "Post destroy successfully"
+    redirect_to posts_path
+  end
+
+  private
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content, :address, category_ids: [])
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,7 @@ class Post < ApplicationRecord
 
   has_many :post_category_ships
   has_many :categories, through: :post_category_ships
+
+
+
 end
