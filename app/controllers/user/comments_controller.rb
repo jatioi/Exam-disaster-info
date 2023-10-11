20e2961b70de7be87dec05bfbe0b797296e12474
Jatioi <Jsuzibloguen@gmail.com>
2023-10-11 21:18:56 +0800

    added: pagination to user/comments

--- app/controllers/user/comments_controller.rb
+++ app/controllers/user/comments_controller.rb
@@ -2,6 +2,6 @@ class User::CommentsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @my_comments = current_user.comments
+    @my_comments = current_user.comments.page(params[:page])&.per(10)
   end
 end
