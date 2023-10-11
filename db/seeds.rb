ce0eff4e4f7dec7e25dc3e7b80ccd719ed4d53e7
Jatioi <Jsuzibloguen@gmail.com>
2023-10-11 15:47:16 +0800

    added: seeds for faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do
+  user = User.create!(
+    email: Faker::Internet.email,
+    password: '123123',
+    password_confirmation: "123123")
+  puts "create user email: #{user.email}"
+end
+
+categories = %w[Tsunami, Earthquake, Tornado]
+categories.each do |name|
+  Category.find_or_create_by(name: name)
+  puts "Created category: #{name}"
+end
+
+30.times do |i|
+  puts "start create #{i} post"
+  user = User.all.sample
+  post = Post.create(
+    title: Faker::Lorem.sentence,
+    content: Faker::Lorem.paragraph,
+    user: user,
+    categories: Category.all.sample(rand(0..5)),
+    address: Faker::Address.full_address
+  )
+  puts "Created post: #{post.title}"
+  rand(0..50).times do
+    Comment.create(
+      content: Faker::Lorem.sentence,
+      user: User.all.sample,
+      post: post
+    )
+  end
+  puts "Finish creating post #{i}"
+end
+
+
+
