558fb565390429a151384938e175fd5600c0a32e
Jatioi <Jsuzibloguen@gmail.com>
2023-10-12 15:12:57 +0800

    adjusted user/posts pagination to per 5

--- app/controllers/user/posts_controller.rb
+++ app/controllers/user/posts_controller.rb
@@ -2,6 +2,6 @@ class User::PostsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @user_posts = current_user.posts.all.page(params[:page])&.per(10)
+    @user_posts = current_user.posts.all.page(params[:page])&.per(5)
   end
 end
